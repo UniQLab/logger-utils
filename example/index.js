a1bd59cf9918333fa401e245a056fc7ea9d48fb4
Alex Dadukin <ald.games.2014@outlook.com>
2016-04-05 22:40:29 +0300

    Reaction on date changed

--- example/index.js
+++ example/index.js
@@ -3,9 +3,11 @@ const loggerFactory = require('../');
 const logger = loggerFactory.createRequestsLogger('test', __dirname);
 const path = require('path');
 
-logger.info('hello world!');
-logger.info('hello world2!');
-logger.info('hello world3!');
+// logger.info('hello world!');
+// logger.info('hello world2!');
+// logger.info('hello world3!');
 
-const logger2 = loggerFactory.createErrorLogger(process.env.LOG || 'test', { logIntoFile: true, logIntoConsole: true });
-logger2.error('hello world4!');
+const logger2 = loggerFactory.createErrorLogger(process.env.LOG || 'test', { pathToLogsFolder: __dirname, logIntoFile: true, logIntoConsole: true });
+setInterval(() => {
+	logger2.error('hello world4!');
+}, 5000);
