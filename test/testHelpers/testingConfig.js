0842a0aecf62a20723ccbaa999e58a6c49a53703
flyingcartman <beyondbirthday90@gmail.com>
2016-04-06 23:11:45 +0300

    git status

--- test/testHelpers/testingConfig.js
+++ test/testHelpers/testingConfig.js
@@ -3,5 +3,8 @@
 const path = require('path');
 
 module.exports = {
-    PATH_TO_LOGS_FOLDER: path.join(__dirname, '../../logs')
+    PATH_TO_LOGS_FOLDER: path.join(__dirname, '../../logs'),
+
+    FILE_TRANSPORT_STRING: 'query => {\n\t\t\tfs.createWriteStream(this.pathService.build(), {\n\t\t\t\tflags: \'a\',\n\t\t\t\tencoding: \'utf8\',\n\t\t\t\tmode: 0o666\n\t\t\t}).write(`${query.output}\\n`);\n\t\t}',
+    CONSOLE_TRANSPORT_STRING: 'query => console.log(query.output)'
 };
